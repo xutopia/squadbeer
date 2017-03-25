8f68ded2be2cd0d1776481da3c19da630ef8310f
xutopia <xuology@gmail.com>
2017-03-25 17:05:05 -0400

    Set up boiler plate for food-beer and beer-food components

---
+++ src/containers/BeervsFood.jsx
@@ -0,0 +1,3 @@
+// Component for requesting specific beers ordered and how many of each type of food was ordered with said beer
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
