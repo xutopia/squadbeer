418b8fc985a7a731097e538fe267f5b92293b546
xutopia <xuology@gmail.com>
2017-03-25 17:20:45 -0400

    complete boilerplate components for food-beer charts

--- src/containers/FoodvsBeer.jsx
+++ src/containers/FoodvsBeer.jsx
@@ -1,1 +1,19 @@
 // Component for requesting specific foods ordered and how many of each type of beer was ordered with said food
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class FoodToBeer extends Component {
+  render() {
+    return (
+      <div>
+        Food to Beer chart
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { state };
+}
+
+export default connect(mapStateToProps)(FoodToBeer);
