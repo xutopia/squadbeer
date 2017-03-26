03ea25f8e82c8569d61082e2f43ccb0837f31736
Eric Lau <elau89@gmail.com>
2017-03-25 23:32:21 -0400

    remove head

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-<<<<<<< HEAD
 import BarSpecificChart from './containers/barSpecificChart.js'
 import BeerToFood from './containers/BeervsFood.js';
 import InputExternal from './containers/inputExternal.js'
