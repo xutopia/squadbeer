3df1d453c44e2ea916bb811770fad86fcf5bd935
xutopia <xuology@gmail.com>
2017-03-25 21:27:38 -0400

    update inputExternal to take a function that will dispatch an action

--- src/containers/BeervsFood.js
+++ src/containers/BeervsFood.js
@@ -2,15 +2,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import InputExternal from './inputExternal';
-import BarSpecificChart from './barSpecificChart';
+import { Bar } from 'react-chartjs-2';
 
 class BeerToFood extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputChange: ''
+    };
+  }
+
+  handleInputChange(state, event) {
+    const text = event.target.value;
+  }
+
   render() {
     return (
       <div>
         What kinds of food do people order with this kind of beer?
         <InputExternal />
-        <BarSpecificChart />
       </div>
     )
   }
