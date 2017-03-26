2110d9b86d913aae9550f8057655a8f187fe0ec4
xutopia <xuology@gmail.com>
2017-03-26 00:43:02 -0400

    resolve merge conflicts

---
+++ src/actions/actionFile.js
@@ -0,0 +1,7 @@
+// all actions for all components
+import axios from 'axios';
+import {
+  ROOT_URL,
+  FETCH_BEERS_FROM_FOOD,
+  FETCH_FOODS_FROM_BEER
+} from './actionTypes';
---
+++ src/actions/actionTypes.js
@@ -0,0 +1,4 @@
+// list of action types constants
+export const ROOT_URL = '/api/';
+export const FETCH_BEERS_FROM_FOOD = 'FETCH_BEERS_FROM_FOOD';
+export const FETCH_FOODS_FROM_BEER = 'FETCH_FOODS_FROM_BEER';
