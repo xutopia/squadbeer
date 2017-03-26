5b30d4850ad205a0acd3adc6214dc7e32ff7d3b6
xutopia <xuology@gmail.com>
2017-03-25 23:13:00 -0400

    create 2 actions that fetch beer data based on food and food data based on beer

--- src/actions/actionFile.js
+++ src/actions/actionFile.js
@@ -5,3 +5,21 @@ import {
   FETCH_BEERS_FROM_FOOD,
   FETCH_FOODS_FROM_BEER
 } from './actionTypes';
+
+export const fetchBeersByFood = (food) => {
+  const request = axios.get(`${ROOT_URL}/${food}`);
+
+  return {
+    type: FETCH_BEERS_FROM_FOOD,
+    payload: request
+  };
+}
+
+export const fetchFoodsByBeer = (beer) => {
+  const request = axios.get(`${ROOT_URL}/${beer}`);
+
+  return {
+    type: FETCH_FOODS_FROM_BEER,
+    payload: request
+  };
+}
